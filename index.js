318d95124de80b69c28c49850d028fbe51a2137f
delightful-range-7379 <khaled.khalifeh@gmail.com>
2018-12-09 12:22:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function getFirstSelector(selector) {
+  var element = document.querySelector(selector);
+return element;
+}
+
+function nestedTarget() {
+  var target = document.querySelector(`#nested .target`);
+  return target;
+}
+
+function increaseRankBy(n) {
+  var rankList = document.querySelectorAll(`ul.ranked-list li`);
+  for (var i=0; i<rankList.length; i++) {
+    rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + n;
+  }
+}
 
